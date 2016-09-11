b825449efa2c6e01a97a3a655dd23401267eba24
Takeshi Miyajima <rozyhead@gmail.com>
2016-09-12 00:41:02 +0900

    WIP

---
+++ haru-engine/src/main/java/haru/engine/HaruEngine.java
@@ -0,0 +1,16 @@
+package haru.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author takeshi
+ */
+@SpringBootApplication
+public class HaruEngine {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HaruEngine.class, args);
+  }
+
+}
---
+++ haru-worker/src/main/java/haru/worker/HaruWorker.java
@@ -0,0 +1,16 @@
+package haru.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author takeshi
+ */
+@SpringBootApplication
+public class HaruWorker {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HaruWorker.class, args);
+  }
+
+}
